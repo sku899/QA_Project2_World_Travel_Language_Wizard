06d31c97211c26f3d7d907b1e81c570831e723be
Harry Volker <harryvolker@Harrys-MBP.connect>
2020-09-09 16:38:55 +0100

    colour variable fix

--- frontend/app.py
+++ frontend/app.py
@@ -9,7 +9,7 @@ def home():
     hostname = getenv("HOSTNAME")
     backend_hostname = requests.get("http://backend:5001/hostname")
     random = requests.get("http://backend:5001/random")
-    colour = blue
+    colour = "blue"
     return f"<body style='background-color:{colour};'>\n<h1>Hello friend.</h1>\n\n<h2>I'm currently running in {hostname}.</h2>\n\n<h2>The backend I'm chatting with is running in {backend_hostname.text}</h2>\n\n<h3>The backend gave me this to show you: {random.text}</h3>"
 
 if __name__=="__main__":
