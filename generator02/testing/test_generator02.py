806b981a5856acdb80a388f05f8c6ac747ec6625
SabinaKu <sku@qa.com>
2021-02-27 20:32:11 +0000

    finalise and test code

--- generator02/testing/test_generator02.py
+++ generator02/testing/test_generator02.py
@@ -9,12 +9,12 @@ class TestBase(TestCase):
         return app
 
 class TestResponse(TestBase):
-    def rand_word(self):
+    def random_word(self):
         country = [b"1", b"2", b"3", b"4"]
         response = self.client.get(url_for("random_generator"))
         self.assertIn(response.data, country)
 
-    def test_country(self):
+    def test_word(self):
         with patch("requests.get") as g:
             g.return_value.text = b"1"
             response = self.client.get(url_for("random_generator"))
