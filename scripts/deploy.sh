1b3fdab31cbd0090ca164169be233a7aa0289614
sabinaku <77272450+sabinaku@users.noreply.github.com>
2021-03-01 22:38:39 +0000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,10 @@
 ssh -i ./ssh/id_rsa rsku888@35.247.113.36 << EOF
 ## ssh -i ~/.ssh/jenkins_agent_key swarmanager << EOF
 cat docker-compose-stack.yaml
-docker stack deploy --prune --with-registry-auth --resolve-image=always --compose-file docker-compose-stack.yaml
+#docker stack deploy --prune --with-registry-auth --resolve-image=always --compose-file docker-compose-stack.yaml
 #sudo rm -rf World_Travel_Language_Wizard
 #git clone https://github.com/sabinaku/World_Travel_Language_Wizard.git
 #cd World_Travel_Language_Wizard docker-compose-stack.yaml
 
-#sudo docker stack deploy --compose-file docker-compose-stack.yaml myapp
+sudo docker stack deploy --compose-file docker-compose-stack.yaml myapp
 EOF
