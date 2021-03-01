70f52ded36875bad59530d3b9c99eba4b224e5de
sabinaku <77272450+sabinaku@users.noreply.github.com>
2021-03-01 20:24:38 +0000

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,21 +2,21 @@
 pwd
 echo 'here I am'
 cd ./frontend
-pip3 install -r requirements.txt
+#pip3 install -r requirements.txt
 python3 -m pytest --cov app 
 cd ..
 
 cd ./backend
-pip3 install -r requirements.txt
+#pip3 install -r requirements.txt
 python3 -m pytest --cov app
 cd ..
 
 cd ./generator01
-pip3 install -r requirements.txt
+#pip3 install -r requirements.txt
 python3 -m pytest --cov app
 cd ..
 
 cd ./generator02
-pip3 install -r requirements.txt
+#pip3 install -r requirements.txt
 python3 -m pytest --cov app
 cd ..
