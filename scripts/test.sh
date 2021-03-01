8eb1b148d9303dd1e9812b6871056fff01280983
sabinaku <77272450+sabinaku@users.noreply.github.com>
2021-03-01 17:27:00 +0000

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,13 +7,16 @@ python3 -m pytest --cov app
 cd ..
 
 cd ./backend
+pip3 install -r requirements.txt
 python3 -m pytest --cov app
 cd ..
 
 cd ./generator01
+pip3 install -r requirements.txt
 python3 -m pytest --cov app
 cd ..
 
 cd ./generator02
+pip3 install -r requirements.txt
 python3 -m pytest --cov app
 cd ..
