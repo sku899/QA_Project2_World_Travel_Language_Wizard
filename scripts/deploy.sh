5ecc7886a51a446e3c9a343881ef410183ed6a14
sabinaku <77272450+sabinaku@users.noreply.github.com>
2021-03-01 16:05:41 +0000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scp -i ./ssh/id_rsa /home/jenkins/.jenkins/workspace/project2pipeline/docker-compose.yaml jenkins@35.247.113.36:docker-compose.yaml
+scp -i ./ssh/id_rsa /home/jenkins/.jenkins/workspace/project2pipeline/docker-compose.yaml jenkins@35.247.113.36:docker-compose-stack.yaml
 ssh -i ./ssh/id_rsa jenkins@35.247.113.36 << EOF
-docker stack deploy --compose-file docker-compose.yaml 
+docker stack deploy --compose-file docker-compose-stack.yaml 
 EOF
